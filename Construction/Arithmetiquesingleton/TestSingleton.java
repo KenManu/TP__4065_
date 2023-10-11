5cf9937cd95c9b93d3886d5812489210a0a73dab
KenManu <104905755+KenManu@users.noreply.github.com>
2023-10-11 16:54:55 +0100

    singleton

---
+++ Construction/Arithmetiquesingleton/TestSingleton.java
@@ -0,0 +1,24 @@
+package Construction.Arithmetiquesingleton;
+
+public class TestSingleton {
+    public static void main(String[] args) {
+
+        int som = Arithmetique.getInstance().somme(2, 5);
+        System.out.printf("la somme est %d \n",som);
+
+        int prod = Arithmetique.getInstance().produit(2, 5);
+        System.out.printf("le produit est %d \n",prod);
+
+        int soust = Arithmetique.getInstance().soustraction(2, 5);
+        System.out.printf("la soustraction est %d \n",soust);
+
+        float moy = Arithmetique.getInstance().moyenne(2, 5);
+        System.out.printf("la moyenne est %f \n",moy);
+
+        Arithmetique s1 = Arithmetique.getInstance(8, 3);
+        s1.affiche();
+
+        Arithmetique s2 = Arithmetique.getInstance(5, 9);
+        s2.affiche();
+    }
+}
